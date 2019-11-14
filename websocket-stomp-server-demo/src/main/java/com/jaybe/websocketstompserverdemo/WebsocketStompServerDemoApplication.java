9fbe9b4d377d325392ae995c36034359918e9e7e
Гришаев Илья Андреевич <i.grishaev@csat.ru>
2019-11-14 16:22:47 +0300

    Initial commit

---
+++ websocket-stomp-server-demo/src/main/java/com/jaybe/websocketstompserverdemo/WebsocketStompServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jaybe.websocketstompserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketStompServerDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketStompServerDemoApplication.class, args);
+    }
+
+}
---
+++ websocket-stomp-server-demo/src/test/java/com/jaybe/websocketstompserverdemo/WebsocketStompServerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaybe.websocketstompserverdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketStompServerDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
